63a4cac31356eb0acce932be8809aa8974bee445
Raghu-Ch <raghu6004@gmail.com>
2017-02-01 22:36:47 -0800

    added logic to get quiz questions and answers

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -14,9 +14,35 @@
           vm.dataServices = DataServices;
           vm.questionAnswered = questionAnswered;
           vm.activeQuestion = 0;
+          vm.setActiveQuestion = setActiveQuestion;
+
+          var numQuestionsAnswered = 0;
+
+          function setActiveQuestion () {
+            var breakOut = false;
+            var quizLength = DataServices.quizQuestions.length - 1;
+
+            while (!breakOut) {
+                  vm.activeQuestion = vm.activeQuestion < quizLength?++vm.activeQuestion:0;
+
+                  if (DataServices.quizQuestions[activeQuestion].selected === null)
+                  breakOut = true;
+            }
+          }
 
           function questionAnswered () {
-            
+
+            var quizLength = DataServices.quizQuestions.length;
+
+            if (DataServices.quizQuestions[vm.activeQuestion].selected !== null){
+                numQuestionsAnswered++;
+
+                if (numQuestionsAnswered >= quizLength) {
+                     // Finilise the quiz
+                }
+            }
+
+            vm.setActiveQuestion();
           }
       }
 
